5bc80fad0394f41e976e1c8606be78dcba53bbac
Leo Harrison <leo.harrison96@gmail.com>
2023-02-01 10:54:55 -0700

    update json

--- js/temple_list.js
+++ js/temple_list.js
@@ -2,18 +2,48 @@ let jsonURL = 'https://leoh906.github.io/wdd230finalproject/json_folder/temples.
 
 async function getTemples(jsonURL) {
 
-    const response = await fetch(jsonURL);
-    console.log(response);
+    const responseItem = await fetch(jsonURL);
+    console.log(responseItem)
 
-    if(response.ok) {
-        const jsResponse = await response.json()
+
+    if(responseItem.ok) {
+        const jsResponse = await responseItem.json()
         console.log(jsResponse)
+        const templeDirectory = jsResponse['temples']
+        // To get the values of the object, use the format: templeDirectory._
+
+        templeDirectory.forEach(element => {
+            let container = document.createElement('div')
+            let templeImage =document.createElement('img')
+            let firstText = document.createElement('p')
+            let secondText = document.createElement('p')
+            let thirdText = document.createElement('p')
+            let firstButton = document.createElement('button')
+            let secondButton = document.createElement('button')
+            let thirdButton = document.createElement('button')
+    
+            container.setAttribute('id',container)
+            container.appendChild(templeImage)
+            templeImage.setAttribute('src',element.picture)
+
+
+
+
+        })
+
+      
+
+
+
+
+
+
     }
 
 
 }
 
-getTemples()
+getTemples(jsonURL)
 
 
 
